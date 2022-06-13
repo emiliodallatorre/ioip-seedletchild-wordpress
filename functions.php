543a0788ae59190040740827eb96c6f20d07f397
Emilio Dalla Torre <info@emiliodallatorre.it>
2022-06-13 14:31:49 +0200

    Prima bozza ultimata.

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ function twentytwentyone_styles()
 {
     wp_enqueue_style('child-style', get_stylesheet_uri(),
         array('twenty-twenty-one-style'), wp_get_theme()->get('Version'));
+    # Font
+    wp_enqueue_style('jah-google-fonts', 'https://fonts.googleapis.com/css2?family=Just+Another+Hand&display=swap', false);
 }
 
 add_action('wp_enqueue_scripts', 'twentytwentyone_styles');
